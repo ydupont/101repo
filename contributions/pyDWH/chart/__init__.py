d8e828f7fc11c15a3906dccfeb7a0e988025994d
Yves Dupont <ydupont@uni-koblenz.de>
2013-08-05 17:10:18 +0200

    added Contribution:pyDWH

---
+++ contributions/pyDWH/chart/__init__.py
@@ -0,0 +1,17 @@
+# Constant for x-axis
+TIME = "time"
+
+# Constant for y-axis
+SALARY = "salary"
+
+# Constants for salary value
+AVERAGE = "average"
+MEDIAN = "median"
+TOTAL = "total"
+
+# Constants for lines
+AGE = "age"
+COMPANY = "company"
+GENDER = "gender"
+JOBROLE = "jobrole"
+MANAGER = "manager"
